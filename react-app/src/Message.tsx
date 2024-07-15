0f6f6a1c52c923253cba13d3580e70a24b4227d5
Charindu Liyanage <charinduliyanage17@gmail.com>
2024-07-16 00:24:44 +0530

    bootstrap

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+//PascalCasing
+function Message(){
+//jsx-Javascript XML
+const name = 'Charindu';
+//you can dynamically change anything also the name here
+//using this syntax we can easilly describe the interface of the application
+return <h1>Hello {name}</h1>
+}
+export default Message;
--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
 
+//react DOM is used to  render this component tree(Sctrict mode) inside the element root
+//what render is basically does is displaying  the root inside it
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
